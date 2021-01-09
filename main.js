d1b531eae986bda9c71760a7fff8dfd4c8c2e72f
CodinGitHub <gms2programador@gmail.com>
2021-01-09 06:18:13 -0500

    variables

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-// alert("Bienvenidos a CodingTube");
+/*
+let age;
+let birthYear;
+edad = prompt("Introduce tu edad");
+document.write(`Tu tienes ${edad} años`);
+birthYear = 2021 - edad;
+document.write(`Tu naciste en el año ${birthYear}`);
+*/
 
-/*alert(1+
-    3
-    +6)*/
+let userAge;
+let birthYear;
 
-alert("Hola");
-["Bienvenidos a", "CodingTube"].forEach(alert)
+userAge = prompt("Ingresa tu edad por favor:");
+
+birthYear = 2022 - userAge;
+
+document.write(`Tu tienes ${userAge} años de edad, y naciste en el año ${birthYear}.`);
